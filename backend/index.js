eb1469ed924e8eb4daff890565e468f9c9a6bcad
Methilan-R <rmethilan@gmail.com>
2025-08-13 10:35:43 +0530

    database Connection

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,14 @@
 import express from 'express'
 import cors from 'cors'
+import ConnectedToDB from './db/db.js'
+import dotenv from 'dotenv'
 
+dotenv.config()
 const app = express()
 app.use(cors())
 app.use(express.json())
+
+ConnectedToDB()
 
 app.listen(5000,()=>{
     console.log("Server Running on 5000")
