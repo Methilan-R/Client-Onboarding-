c62f678a21ee0fb3aa130c0f30824f62ea9aaf77
Methilan-R <rmethilan@gmail.com>
2025-08-13 10:28:11 +0530

    index setup

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.listen(5000,()=>{
+    console.log("Server Running on 5000")
+})
+
+
+
