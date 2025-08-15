58dbd7cf59a8cd33340f018bae15c20ac37b089d
Methilan-R <rmethilan@gmail.com>
2025-08-15 16:13:37 +0530

    backend

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,12 @@ import dotenv from 'dotenv'
 import clientRoutes from './routes/client.js'
 dotenv.config()
 const app = express()
-app.use(cors({ origin: "*" }));
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 app.use(express.json())
 
 app.use('/api',clientRoutes)
